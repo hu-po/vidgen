9b0ba898f14036432b28abc877fd53b09be5fe5a
hu.po <hugo.p.cmu@gmail.com>
2023-12-19 13:03:15 -0600

    models sourced

--- run.py
+++ run.py
@@ -1,16 +1,61 @@
 """
 conda create -n vid2vid python=3.9
 conda activate vid2vid
+pip install replicate
 
-# https://replicate.com/jagilley/controlnet-pose
-docker run --name controlnet_container r8.im/jagilley/controlnet-pose@sha256:0304f7f774ba7341ef754231f794b1ba3d129e3c46af3022241325ae0c50fb99
-docker commit controlnet_container controlnet_container
+https://replicate.com/stability-ai/stable-video-diffusion
+docker run --name svd r8.im/stability-ai/stable-video-diffusion@sha256:3f0457e4619daac51203dedb472816fd4af51f3149fa7a9e0b5ffcf1b8172438
+docker commit svd svd
+"input": {
+    "cond_aug": 0.02,
+    "decoding_t": 7,
+    "input_image": "https://replicate.delivery/pbxt/JvLi9smWKKDfQpylBYosqQRfPKZPntuAziesp0VuPjidq61n/rocket.png",
+    "video_length": "14_frames_with_svd",
+    "sizing_strategy": "maintain_aspect_ratio",
+    "motion_bucket_id": 127,
+    "frames_per_second": 6
+}
 
+https://replicate.com/stability-ai/sdxl
+docker run --name sdxl r8.im/stability-ai/sdxl@sha256:39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b
+docker commit sdxl sdxl
+"input": {
+    "width": 768,
+    "height": 768,
+    "prompt": "An astronaut riding a rainbow unicorn, cinematic, dramatic",
+    "refine": "expert_ensemble_refiner",
+    "scheduler": "K_EULER",
+    "lora_scale": 0.6,
+    "num_outputs": 1,
+    "guidance_scale": 7.5,
+    "apply_watermark": false,
+    "high_noise_frac": 0.8,
+    "negative_prompt": "",
+    "prompt_strength": 0.8,
+    "num_inference_steps": 25
+}
 
+https://replicate.com/pollinations/music-gen
+docker run --name musicgen_container r8.im/pollinations/music-gen@sha256:9b8643c06debace10b9026f94dcb117f61dc1fee66558a09cde4cfbf51bcced6
+docker commit musicgen_container musicgen_container
+"input": {
+    "text": "cool trendy bass heavy electronic music that would fit nicely in an epic movie trailer",
+    "duration": 12
+}
 
-# https://replicate.com/cjwbw/rembg
-docker run --name bgremoval_container r8.im/cjwbw/rembg@sha256:fb8af171cfa1616ddcf1242c093f9c46bcada5ad4cf6f2fbe8b81b330ec5c003
-docker commit bgremoval_container bgremoval_container
+https://replicate.com/mistralai/mixtral-8x7b-instruct-v0.1
+docker run --name mixtral r8.im/mistralai/mixtral-8x7b-instruct-v0.1@sha256:2b56576fcfbe32fa0526897d8385dd3fb3d36ba6fd0dbe033c72886b81ade93e
+docker commit mixtral mixtral
+"input": {
+    "top_k": 50,
+    "top_p": 0.9,
+    "prompt": "Write a bedtime story about neural networks I can read to my toddler",
+    "temperature": 0.6,
+    "max_new_tokens": 1024,
+    "prompt_template": "<s>[INST] {prompt} [/INST] ",
+    "presence_penalty": 0,
+    "frequency_penalty": 0
+}
 """
 
 import base64
