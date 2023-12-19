f6d809a9d3e4f9d26a339803c45959c02db39b72
hu.po <hugo.p.cmu@gmail.com>
2023-12-19 16:15:05 -0600

    main structure

--- run.py
+++ run.py
@@ -19,6 +19,7 @@ docker commit svd svd
 https://replicate.com/stability-ai/sdxl
 docker run --name sdxl r8.im/stability-ai/sdxl@sha256:39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b
 docker commit sdxl sdxl
+docker run -p 5000:5000 --gpus=all sdxl
 "input": {
     "width": 768,
     "height": 768,
@@ -78,192 +79,117 @@ def nuke_docker():
         os.system(f"docker rm {containers}")
     os.system("docker container prune -f")
 
-
-def crop_center_square(input_video_path):
-    # Get video details using ffprobe
-    cmd = [
-        "ffprobe",
-        "-v",
-        "error",
-        "-select_streams",
-        "v:0",
-        "-show_entries",
-        "stream=width,height,r_frame_rate",
-        "-of",
-        "csv=p=0",
-        input_video_path,
-    ]
-    output = subprocess.check_output(cmd).decode("utf-8").strip().split(",")
-    width, height, framerate = [int(output[0]), int(output[1]), output[2].split("/")[0]]
-    min_dim = min(width, height)
-
-    # Calculate the offset for cropping
-    x_offset = (width - min_dim) // 2
-    y_offset = (height - min_dim) // 2
-
-    # Set output file name
-    output_video_path = f"test.square.30fps.{min_dim}x{min_dim}.mp4"
-
-    # Use ffmpeg to crop the video
-    cmd = [
-        "ffmpeg",
-        "-i",
-        input_video_path,
-        "-vf",
-        f"crop={min_dim}:{min_dim}:{x_offset}:{y_offset}",
-        "-c:v",
-        "libx264",
-        "-r",
-        "30",
-        output_video_path,
-    ]
-    subprocess.call(cmd)
-
-    print(f"Video cropped and saved to: {output_video_path}")
-
-
-def extract_frame_number(frame_path):
-    # Extract the number from the filename, assuming the filename format is controlnet_pose_<number>.png
-    frame_name = frame_path.split("/")[-1]
-    return int(frame_name.split("_")[-1].split(".")[0])
-
-
-def process_video_frames(
-    input_video_path: str = "/home/oop/dev/simicam/data/test.square.30fps.1080x1080.mp4",
-    base_output_dir: str = "/home/oop/dev/simicam/logs/",
-    output_video_filename: str = "out.mp4",
-    prompt: str = "humanoid robot dancing test, unreal engine, 8k render",
-    fps: int = 30,
-    docker_url: str = "http://localhost:5000/predictions",
-    **kwargs,
-):
-    # Generate a unique id for this generation session
-    session_id = str(uuid.uuid4())[:6]
-
-    # Create a output folder for the session id and use that as the output dir
-    output_dir = os.path.join(base_output_dir, session_id)
-    os.makedirs(output_dir, exist_ok=True)
-
-    # Extract frames from video using ffmpeg
-    os.system(f"ffmpeg -i {input_video_path} -vf fps={fps} {output_dir}/raw_%05d.png")
-
-    # Run the controlnet docker container and remove it after use
+def make_docker(name: str):
     nuke_docker()
-    docker_process = subprocess.Popen(
-        [
-            "docker",
-            "run",
-            "--rm",
-            "-p",
-            "5000:5000",
-            "--gpus=all",
-            "controlnet_container",
-        ]
-    )
-    time.sleep(30)  # Let the docker container startup
-
-    # Feed each frame to the controlnet docker container and save the output image
-    for i, frame_path in enumerate(
-        sorted(
-            glob.glob(os.path.join(output_dir, "raw_*.png")), key=extract_frame_number
-        )
-    ):
-        with open(frame_path, "rb") as img_file:
-            response = requests.post(
-                docker_url,
-                headers={"Content-Type": "application/json"},
-                json={
-                    "input": {
-                        "image": f"data:image/png;base64,{base64.b64encode(img_file.read()).decode('utf-8')}",
-                        "prompt": prompt,
-                        "seed": 42,
-                    },
-                },
-            )
-        img = Image.open(
-            BytesIO(base64.b64decode(response.json()["output"][0].split(",")[1]))
-        )
-        img.save(os.path.join(output_dir, f"controlnet_pose_{i:05}.png"))
-        img = Image.open(
-            BytesIO(base64.b64decode(response.json()["output"][1].split(",")[1]))
-        )
-        img.save(os.path.join(output_dir, f"controlnet_full_{i:05}.png"))
-
-    # Kill controlnet docker container
-    docker_process.terminate()
-
-    # Run the bgremoval docker container
-    nuke_docker()
-    docker_process = subprocess.Popen(
-        [
-            "docker",
-            "run",
-            "--rm",
-            "-p",
-            "5000:5000",
-            "--gpus=all",
-            "bgremoval_container",
-        ]
-    )
+    docker_process = subprocess.Popen([ "docker", "run", "--rm", "-p", "5000:5000", "--gpus=all", name])
     time.sleep(20)  # Let the docker container startup
+    return docker_process
 
-    # Feed each frame to the bgremoval docker container
-    for i, frame_path in enumerate(
-        sorted(
-            glob.glob(os.path.join(output_dir, "controlnet_full_*.png")),
-            key=extract_frame_number,
+
+def make_short(
+    output_dir: str = "/home/oop/dev/data",
+    output_video_filename: str = "test_short.mp4",
+    prompt: str = "short story about a white bengal cat",  
+):
+    # ---- MIXTRAL
+    docker_process = make_docker("mixtral")
+    response = requests.post(
+        "http://localhost:5000/predictions",
+        headers={"Content-Type": "application/json"},
+        json={
+                "input": {
+                    "prompt": f"Write a series of short scene descriptions for a movie trailer about {prompt}, separate each scene with newlines. The scene descriptions will be used as prompts for an image generation model.",
+                    "top_k": 50,
+                    "top_p": 0.9,
+                    "temperature": 0.6,
+                    "max_new_tokens": 1024,
+                    "presence_penalty": 0,
+                    "prompt_template": "<s>[INST] {prompt} [/INST] ",
+                    "frequency_penalty": 0,
+                }})
+    scene_prompts = response.json()["output"].split("\n")
+    docker_process.terminate()
+    nuke_docker()
+    # ---- SDXL
+    docker_process = make_docker("sdxl")
+    for i, scene_prompt in enumerate(scene_prompts):
+        response = requests.post(
+            "http://localhost:5000/predictions",
+            headers={"Content-Type": "application/json"},
+            json={
+                "input": {
+                    "width": 768,
+                    "height": 768,
+                    "prompt": scene_prompt,
+                    "refine": "expert_ensemble_refiner",
+                    "scheduler": "K_EULER",
+                    "lora_scale": 0.6,
+                    "num_outputs": 1,
+                    "guidance_scale": 7.5,
+                    "apply_watermark": False,
+                    "high_noise_frac": 0.8,
+                    "negative_prompt": "",
+                    "prompt_strength": 0.8,
+                    "num_inference_steps": 25,
+                }
+            },
         )
-    ):
-        with open(frame_path, "rb") as img_file:
-            response = requests.post(
-                docker_url,
-                headers={"Content-Type": "application/json"},
-                json={
-                    "input": {
-                        "image": f"data:image/png;base64,{base64.b64encode(img_file.read()).decode('utf-8')}",
-                    },
-                },
-            )
         img = Image.open(
             BytesIO(base64.b64decode(response.json()["output"].split(",")[1]))
         )
-        img.save(os.path.join(output_dir, f"nobg_{i:05}.png"))
-
-    # Kill controlnet docker container
+        img.save(os.path.join(output_dir, f"keyframe_{i:05}.png"))
     docker_process.terminate()
-    os.system("docker kill $(docker ps -q)")
-
-    # Combine frames into video
-    os.system(
-        f"ffmpeg -framerate {fps} -i {output_dir}/nobg_%05d.png -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p {base_output_dir}/{output_video_filename}"
+    nuke_docker()
+    # ---- SVD
+    docker_process = make_docker("svd")
+    for i, keyframe_path in enumerate(
+        sorted(
+            glob.glob(os.path.join(output_dir, "keyframe_*.png")),
+            key=extract_frame_number,
+        )
+    ):
+        with open(keyframe_path, "rb") as img_file:
+            response = requests.post(
+                "http://localhost:5000/predictions",
+                headers={"Content-Type": "application/json"},
+                json={
+                    "input": {
+                        "input_image": f"data:image/png;base64,{base64.b64encode(img_file.read()).decode('utf-8')}",
+                        "cond_aug": 0.02,
+                        "decoding_t": 7,
+                        "video_length": "14_frames_with_svd",
+                        "sizing_strategy": "maintain_aspect_ratio",
+                        "motion_bucket_id": 127,
+                        "frames_per_second": 6
+                    },
+                },
+            )
+    
+    # ---- MUSICGEN
+    docker_process = make_docker("musicgen_container")
+    response = requests.post(
+        "http://localhost:5000/predictions",
+        headers={"Content-Type": "application/json"},
+        json={
+            "input": {
+                "text": f"cool trendy bass heavy electronic music that would fit nicely in a movie trailer about {prompt}",
+                "duration": 12,
+            }
+        },
     )
+            
+    # Combine scenes and audio with ffmpeg
+    os.system(f"ffmpeg -r 6 -f image2 -s 768x768 -i {os.path.join(output_dir, 'keyframe_%05d.png')} -i {os.path.join(output_dir, 'audio.wav')} -shortest -c:v libx264 -pix_fmt yuv420p {os.path.join(output_dir, output_video_filename)}")
 
 
 if __name__ == "__main__":
-    # Define the values you want to try for fps and prompt
-    fps_values = [60]
     prompt_values = [
-        # "robot, battle droid, unreal engine",
-        # "shaman world of warcraft, mmo",
-        "anime mma fighter",
-        "captain america in style of moebius",
-        "obama white suit",
-        "elon musk anime",
-        "elon musk pixar",
-        "elon musk moebius",
+        "cute story about a penguin and a polar bear",
+        "horror movie about an evil banana",
     ]
-
-    # Iterate over all combinations of fps and prompt values
-    for fps in fps_values:
-        for prompt in prompt_values:
-            process_video_frames(
-                input_video_path="/home/oop/dev/data/test.square.30fps.1080x1080.mp4",
-                base_output_dir="/home/oop/dev/data/",
-                output_video_filename=f"output_{fps}_{prompt[:5]}.mp4",
-                prompt=prompt,
-                fps=fps,
-                docker_url="http://localhost:5000/predictions",
-            )
-
-    # Call the function with your video path
-    # crop_center_square("/home/oop/Downloads/PXL_20231019_175956589.TS.mp4")
+    for prompt in prompt_values:
+        make_short(
+            output_dir="/home/oop/dev/data/",
+            output_video_filename=f"output_{prompt[:5]}.mp4",
+            prompt=prompt,
+        )
